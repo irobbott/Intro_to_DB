ed399463a5a4014977551d200cfcfb573de9e99a
Iconic Robbott <dev.robbott@gmail.com>
2025-02-02 03:19:07 -0800

    Populating Your Database with Your More Data

---
+++ task_6.sql
@@ -0,0 +1,5 @@
+USE alx_book_store;
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech', '124 Happiness Ave.'),
+       (3, 'Obed Ehoneah', 'eobe@sandtech', '125 Happiness Ave.'),
+       (4, 'Nehemial Kamolu', 'nkamolu@sandtech', '126 Happiness Ave.');
