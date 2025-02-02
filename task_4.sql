44117bb899359d8c2c04eca3d5df99d43f0d6994
Iconic Robbott <dev.robbott@gmail.com>
2025-02-02 11:01:43 +0100

    Full description

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
 
-SELECT *
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, CHARACTER_MAXIMUM_LENGTH
 FROM information_schema.columns
 WHERE table_name = 'books' AND table_schema = 'alx_book_store';
