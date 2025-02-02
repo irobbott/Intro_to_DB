90ec1039a050e21628464b0ecf3c05d419b98bbe
Iconic Robbott <dev.robbott@gmail.com>
2025-02-02 10:37:44 +0100

    List all the tables created

--- task_3.sql
+++ task_3.sql
@@ -1,2 +1,8 @@
-SHOW TABLES
+import mysql.connector
 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    password="yourpassword",
+    database="alx_book_store"
+)
