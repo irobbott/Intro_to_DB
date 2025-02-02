66d85209fbfa49f05fecbe7268719221fd762c20
Iconic Robbott <dev.robbott@gmail.com>
2025-02-02 11:45:00 +0100

    Populating Your Database with Your More Data

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
 INSERT INTO customer (customer_id, customer_name, email, address);
-VALUES ('2', 'Blessing Malik', 'bmalik@sandtech', '124 Happiness Ave.'),
-	('3', 'Obed Ehoneah', 'eobe@sandtech', '125 Happiness Ave.'),
-	('4', 'Nehemial Kamolu', 'nkamolu@sandtech', '126 Happiness Ave.');
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech', '124 Happiness Ave.'),
+	(3, 'Obed Ehoneah', 'eobe@sandtech', '125 Happiness Ave.'),
+	(4, 'Nehemial Kamolu', 'nkamolu@sandtech', '126 Happiness Ave.');
